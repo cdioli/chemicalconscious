3e9f8476b30af8a3f15bd451dd27564f58c1ed20
catherine dioli <cdioli@bates.edu>
2014-05-12 22:20:00 -0400

    added the shampoo page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'page#index'
 
+  get'/shampoo'=>"page#shampoo"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
